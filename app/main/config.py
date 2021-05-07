55c0db8477ae63f66b045785c5be210c59721845
Vitalijus Alšauskas <vitalijus.alsauskas@gmail.com>
2021-05-07 14:32:16 +0300

    connected to azure sql

--- app/main/config.py
+++ app/main/config.py
@@ -1,3 +1,5 @@
+import urllib.parse
+import pyodbc
 class Config:
     SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
     DEBUG = False
@@ -13,8 +15,12 @@ class Config:
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
+
+    params = urllib.parse.quote_plus(
+        "Driver={ODBC Driver 17 for SQL Server};Server=tcp:organizerdb.database.windows.net,1433;Database=organizerdb;Uid=adminas;Pwd=slaptazodis1A.;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://vmyedshomcjtgj:6e0e584bc46342dc27cf732f8e3f7228c81146b448c8744a6dfc4908eb4a728f@ec2-3-91-127-228.compute-1.amazonaws.com:5432/d74an82tip9b3g'
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = True
 
