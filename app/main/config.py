7d227ab6e1568d3b13f49795908060b4624f2fc5
Vitalijus Alšauskas <vitalijus.alsauskas@gmail.com>
2021-05-08 10:51:03 +0300

    Changed to heroku db

--- app/main/config.py
+++ app/main/config.py
@@ -1,5 +1,4 @@
 import urllib.parse
-import pyodbc
 class Config:
     SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
     DEBUG = False
@@ -20,7 +19,7 @@ class DevelopmentConfig(Config):
         "Driver={ODBC Driver 17 for SQL Server};Server=tcp:organizerdb.database.windows.net,1433;Database=organizerdb;Uid=adminas;Pwd=slaptazodis1A.;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
+    SQLALCHEMY_DATABASE_URI = "postgres: // vmyedshomcjtgj: 6e0e584bc46342dc27cf732f8e3f7228c81146b448c8744a6dfc4908eb4a728f@ec2-3-91-127-228.compute-1.amazonaws.com: 5432/d74an82tip9b3g"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = True
 
