0fd146c4b4213ae82bf83e1db8322e01780c01b7
Vitalijus Alšauskas <vitalijus.alsauskas@gmail.com>
2021-05-09 22:24:26 +0300

    changed to postgress

--- app/main/config.py
+++ app/main/config.py
@@ -15,11 +15,8 @@ class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
 
-    params = urllib.parse.quote_plus(
-        "Driver={ODBC Driver 17 for SQL Server};Server=tcp:organizerdb.database.windows.net,1433;Database=organizerdb;Uid=adminas;Pwd=slaptazodis1A.;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgres: // vmyedshomcjtgj: 6e0e584bc46342dc27cf732f8e3f7228c81146b448c8744a6dfc4908eb4a728f@ec2-3-91-127-228.compute-1.amazonaws.com: 5432/d74an82tip9b3g"
+    SQLALCHEMY_DATABASE_URI = 'postgres://vmyedshomcjtgj:6e0e584bc46342dc27cf732f8e3f7228c81146b448c8744a6dfc4908eb4a728f@ec2-3-91-127-228.compute-1.amazonaws.com:5432/d74an82tip9b3g'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_ECHO = True
 
